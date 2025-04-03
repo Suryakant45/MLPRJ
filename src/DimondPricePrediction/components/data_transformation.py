dc7aa8c7a5e945383fcfd65199f505fb31d60513
Suryakanta Sahoo <ssuryakanta696@gmail.com>
2025-04-03 18:12:34 +0530

    model update

---
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+class DataTransformation:
+    def __init__(self):
+        pass
+
+    def get_data_transformation(self):
+        pass
---
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ModelTrainer:
+    def __init__(self):
+        pass
+
+    def initiate_model_training(self):
+        pass
